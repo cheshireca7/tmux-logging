079f072f0f5fb4c7dff0c3952bf2567b0e47cf82
Cheshire Cat <34421496+cheshireca7@users.noreply.github.com>
2024-09-26 13:30:11 +0200

    Update save_complete_history.sh

--- scripts/save_complete_history.sh
+++ scripts/save_complete_history.sh
@@ -10,7 +10,7 @@ main() {
 	if supported_tmux_version_ok; then
 		local file=$(expand_tmux_format_path "${save_complete_history_full_filename}")
 		local history_limit="$(tmux display-message -p -F "#{history_limit}")"
-		tmux capture-pane -J -S "-${history_limit}" -p > "${file}"
+		tmux capture-pane -J -S "-${history_limit}" -p >> "${file}"
 		remove_empty_lines_from_end_of_file "${file}"
 		display_message "History saved to ${file}"
 	fi
